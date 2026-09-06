54393a2eba90718a23a2951f57c757db54904713
READTalk <redtalk.app@hotmail.com>
2026-09-06 13:40:57 +0700

    Enable splitRouteModules and remove v8_passThroughRequests

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,10 +2,9 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
 	ssr: true,
+	splitRouteModules: true,
 	future: {
 		v8_middleware: true,
-		
-		v8_passThroughRequests: true,
 		v8_trailingSlashAwareDataRequests: true,
 	},
 } satisfies Config;
