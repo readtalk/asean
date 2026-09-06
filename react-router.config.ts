477542a082aa04f502456c5c28c7f3584a209cec
READTalk <redtalk.app@hotmail.com>
2026-09-06 13:43:28 +0700

    Simplify future configuration in router settings

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,8 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
 	ssr: true,
 	splitRouteModules: true,
-	future: {
-		v8_middleware: true,
-		v8_trailingSlashAwareDataRequests: true,
-	},
+	future: {},
 } satisfies Config;
